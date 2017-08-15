01b192667e6c68c41002eb78ed8969e9663e3270
AlbusPercivalWulfricBrianDumbeldore <30649232+AlbusPercivalWulfricBrianDumbeldore@users.noreply.github.com>
2017-08-15 18:36:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,8 +5,19 @@ var element = document.getElementById('main-text');
 
 element.innerHTML='new value';
 
+//Change the text of main-text div
+var element = document.getElementById('main-text');
+
+element.innerHTML='new value';
+
 // Move the Image
 var img= document.getElementById('pic');
-img.onclick = function(){
-    img.style.marginLeft="150px";
+var leftMargin=0;
+function moveRight(){
+    leftMargin=leftMargin+10;
+    img.style.leftMargin=leftMargin+ "px";
+}
+img.onclick= function(){
+    var interval=setInterval(moveRight, 100);
 };
+   
