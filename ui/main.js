15939c3552d45142e96e6ff432bd0f3ebae730a0
AlbusPercivalWulfricBrianDumbeldore <30649232+AlbusPercivalWulfricBrianDumbeldore@users.noreply.github.com>
2017-08-16 18:01:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,3 +22,22 @@ button.onclick= function(){
   request.send(null);
   
 };
+
+
+//Submit name
+var nameInput= document.getElementById('name');
+var name= varInput.value;
+var submit= document.getElementById('submit_btn');
+submit.onclick= function(){
+  //Make a request to the server and send the name
+  
+  //Capture a list of names and render it as a list
+  var names=['Name1', 'Name2', 'Name3', 'Name4'];
+  var list='';
+  for (var i=0' i< names.length; i++ ){
+      list+= '<li>'+ names[i] + </li>
+      }
+      var ul= document.getElementById('namelist');
+      ul.innerHTML=list;
+  
+};
