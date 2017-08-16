088d27ff59edd6b6cc89089c8e09b100d4c96c68
AlbusPercivalWulfricBrianDumbeldore <30649232+AlbusPercivalWulfricBrianDumbeldore@users.noreply.github.com>
2017-08-16 17:23:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,7 @@ button.onclick= function(){
             if (request.status===200){
                 var counter= request.responseText;
                 var span=document.getElementById("Count");
-                span.innerHTML=counter.toString();
+                span.innerHTML=Counter.toString();
             }
         }
     };
