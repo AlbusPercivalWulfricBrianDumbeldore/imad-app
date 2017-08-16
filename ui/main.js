d37c7675036d7bc3dd110c2aa06ed97cdbe0a4ba
AlbusPercivalWulfricBrianDumbeldore <30649232+AlbusPercivalWulfricBrianDumbeldore@users.noreply.github.com>
2017-08-16 17:09:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,6 +10,6 @@ button.onclick= function(){
     //Render the variable in the correct span
     counter= counter+1;
     var span= document.getElementById("Count");
-    span.innerHTMl=counter.toString();
+    span.innerHTML=counter.toString();
     
 };
